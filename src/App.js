250a45c4e056e96be8c5742123b198daf74ccb4d
praveen <praveenkumaronamla@gmail.com>
2023-03-11 17:39:17 +0000

    if-else

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {Component} from 'react'
+
+import Welcome from './components/Welcome'
+
+import './App.css'
+
+class App extends Component {
+  state = {
+    isLoggedIn: false,
+  }
+
+  // create a function
+
+  renderAuthButton = () => {
+    const {isLoggedIn} = this.state
+    if (isLoggedIn === true) {
+      return <button type="button">Logout</button>
+    }
+    return <button type="button">Login</button>
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Welcome greeting="Hello" name="User" />
+        {this.renderAuthButton()} {/* calling the function */}
+      </div>
+    )
+  }
+}
+
+export default App
